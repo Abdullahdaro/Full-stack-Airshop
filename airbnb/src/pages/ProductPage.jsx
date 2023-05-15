960d58e218a89647819229c6d253e4fa2e2fb9dd
AbdullahDaro <abode.morx@gmail.com>
2023-05-15 20:33:29 +0300

    add

--- airbnb/src/pages/ProductPage.jsx
+++ airbnb/src/pages/ProductPage.jsx
@@ -26,7 +26,7 @@ const ProductPage = () => {
 
     if (!product) return ''
 
-    const { title} = product;
+    const { title, price, colors, decription, material, age, sex, type, season, size, serialNumber } = product;
 
   return (
     <div>
@@ -34,28 +34,46 @@ const ProductPage = () => {
       {owner && <p>Owner: {owner.photo} </p>}
       
       <div className='flex'>
-        <div className='m-4 gap-4 grid grid-cols-1 md:grid-cols-2 lg:grid-col-3'>
-          <div className="relative w-[50%]">
-            {product.photos?.[0] && (
-              <img src={'http://localhost:4000/uploads/'+product.photos?.[0]} 
-                className='object-cover w-full h-full aspect-w-1 aspect-h-1 ' />
-            )}
-          </div>
-          {product.photos?.[0] && (
-            <img src={'http://localhost:4000/uploads/'+product.photos[1]} />
-          )}
-          {product.photos?.[0] && (
-            <img src={'http://localhost:4000/uploads/'+product.photos[2]} />
-          )}
-          {product.photos?.[0] && (
-            <img src={'http://localhost:4000/uploads/'+product.photos[3]} />
-          )}
+        <div className="m-4 gap-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 w-[80%] ">
+          {product.photos?.length && product.photos.map((photo, index) => (
+            <div className="relative" key={index}>
+              <img
+                src={'http://localhost:4000/uploads/' + photo}
+                className="object-cover w-full h-full aspect-w-1 aspect-h-1"
+                alt={`Product Photo ${index + 1}`}
+              />
+            </div>
+          ))}
         </div>
-        <div className='w-[50%] m-4'>
+        <div className='w-[25%] m-4'>
           <h1>{title}</h1>
+          <h2>Price: {price}$</h2>
+          <h2>{colors}</h2>
+          <div>
+{/*             <h1>Description</h1>
+            <h2>{decription}</h2> */}
+          </div>
+          <div>
+{/*             <h2>Material: {material} </h2> */}
+            <h2>Age: {age}</h2>
+            <h2>ٍSex: {sex}</h2>
+            <h2>Type: {type}</h2>
+            <h2>Season: {season}</h2>
+            <h2>Sizes: {size}</h2>
+          </div>
+          <button>Send a message to the seller</button>
         </div>
       </div>
-     
+      <div>
+        <h1>{title}</h1>
+        <h2>Descritop: <br/>{decription}</h2>
+        <h2>Serial Number: {serialNumber}</h2>
+        <h2>Composition: {material}</h2>
+      </div>
+      <div>
+        <h1>Products that you may like</h1>
+        <div></div>
+      </div>
     </div>
   );
 }
