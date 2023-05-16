ba320b89be175c1bca66a5c90bd4282fc3c42723
AbdullahDaro <abode.morx@gmail.com>
2023-05-16 22:07:00 +0300

    add

--- airbnb/src/pages/ShopsPage/ShopPage.jsx
+++ airbnb/src/pages/ShopsPage/ShopPage.jsx
@@ -1,6 +1,31 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'   
+
+
 
 const ShopPage = () => {
+    const {id} = useParams(); 
+    const [shop, setShop] = useState(null)
+    const [owner, setOwner] = useState(null);
+
+    useEffect(() => {
+        if (!id) {
+            return 
+        }
+        axios.get(`/owner/${id}`).then(response => {
+            setProduct(response.data)
+            const ownerId = response.data.owner;
+            if (ownerId) {
+              // Fetch the owner's data based on the owner ID
+              axios.get(`/owners/${ownerId}`).then(ownerResponse => {
+                setOwner(ownerResponse.data);
+              });
+            }
+        })
+    }, [id])
+
+
   return (
     <div>ShopPage</div>
   )
