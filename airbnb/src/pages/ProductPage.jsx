fe7576c8110bede63118327d7a8b342d47b15c3f
AbdullahDaro <abode.morx@gmail.com>
2023-05-16 21:56:02 +0300

    DD

--- airbnb/src/pages/ProductPage.jsx
+++ airbnb/src/pages/ProductPage.jsx
@@ -39,7 +39,7 @@ const ProductPage = () => {
         {owner && <p>{owner.name}Shop</p>}
       </div>
       <div className='flex '>
-        <div className="m-4 gap-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 w-[80%] ">
+        <div className="my-4 ml-4 gap-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 w-[80%] ">
           {product.photos?.length && product.photos.map((photo, index) => (
             <div className="relative" key={index}>
               <img
@@ -50,29 +50,29 @@ const ProductPage = () => {
             </div>
           ))}
         </div>
-        <div className='w-[25%] m-4 relative p-4'>
-          <div className='flex justify-between mb-12 mt-4'>
+        <div className='w-[25%] my-4 mx-10 relative p-4 rounded-xl '>
+          <div className='flex justify-between mb-6 mt-4'>
             <h1 className='font-second text-xl'>{title}</h1>
             <button className='w-20 text-2xl'><FontAwesomeIcon icon={faRegularHeart} /></button>
           </div>
-          <h2 className='font-second text- mb-12'>Price: {price}$</h2>
+          <h2 className='font-second text- mb-12 text-[#7F8086]'>PRICE: {price}$</h2>
           <div className='mb-4'>
-            <h2>Colors:</h2>
+            <h2>COLORS:</h2>
             {colors.map((color, index) => (
-              <div key={index} className='rounded-full mb-12' style={{ backgroundColor: color, width: '20px', height: '20px' }}></div>
+              <div key={index} className='rounded-full mb-12 border border-black' style={{ backgroundColor: color, width: '20px', height: '20px' }}></div>
             ))}
           </div>
           <div className=' mb-12'>
-            <h2 className='font-second text-ms p-1'>Age: {age}</h2>
-            <h2 className='font-second text-ms p-1'>ٍSex: {sex}</h2>
-            <h2 className='font-second text-ms p-1'>Type: {type}</h2>
-            <h2 className='font-second text-ms p-1'>Season: {season}</h2>
-            <h2 className='font-second text-ms p-1'>Sizes: {size}</h2>
+            <h2 className='font-second text-ms  '>AGE: {age}</h2>
+            <h2 className='font-second text-ms '>SEX: {sex}</h2>
+            <h2 className='font-second text-ms '>TYPE: {type}</h2>
+            <h2 className='font-second text-ms '>SIZES: {size}</h2>
+            <h2 className='font-second text-ms '>SEASON: {season}</h2>
           </div>
-          <button>Send a message to the seller</button>
+          <button className='bg-secondary py-2'>Send a message to the seller</button>
         </div>
       </div>
-      <div>
+      <div className='px-6'>
         <h1>{title}</h1>
         <h2>Descritop: <br/>{decription}</h2>
         <h2>Serial Number: {serialNumber}</h2>
