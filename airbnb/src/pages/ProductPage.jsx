95dcd3c5b64d25e2ef175b7c6f9a9c2b35ea6480
AbdullahDaro <abode.morx@gmail.com>
2023-05-16 20:46:36 +0300

    add

--- airbnb/src/pages/ProductPage.jsx
+++ airbnb/src/pages/ProductPage.jsx
@@ -30,6 +30,7 @@ const ProductPage = () => {
     if (!product) return ''
 
     const { title, price, colors, decription, material, age, sex, type, season, size, serialNumber } = product;
+ // write for me the 
 
   return (
     <div className=''>
@@ -37,36 +38,36 @@ const ProductPage = () => {
         {owner && <p>{owner.photo} </p>}
         {owner && <p>{owner.name}Shop</p>}
       </div>
-      <div className='flex'>
+      <div className='flex '>
         <div className="m-4 gap-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 w-[80%] ">
           {product.photos?.length && product.photos.map((photo, index) => (
             <div className="relative" key={index}>
               <img
                 src={'http://localhost:4000/uploads/' + photo}
-                className="object-cover w-full h-full aspect-w-1 aspect-h-1"
+                className="object-cover w-full h-full aspect-w-1 aspect-h-1 border border-gray-300"
                 alt={`Product Photo ${index + 1}`}
               />
             </div>
           ))}
         </div>
-        <div className='w-[25%] m-4 relative bg-gray-100 p-2 rounded-2xl'>
+        <div className='w-[25%] m-4 relative p-4'>
           <div className='flex justify-between mb-12 mt-4'>
-            <h1 className='font-second text-2xl'>{title}</h1>
+            <h1 className='font-second text-xl'>{title}</h1>
             <button className='w-20 text-2xl'><FontAwesomeIcon icon={faRegularHeart} /></button>
           </div>
-          <h2 className='font-second text-xl mb-12'>Price: {price}$</h2>
+          <h2 className='font-second text- mb-12'>Price: {price}$</h2>
           <div className='mb-4'>
             <h2>Colors:</h2>
             {colors.map((color, index) => (
-              <div key={index} className='rounded-full' style={{ backgroundColor: color, width: '20px', height: '20px' }}></div>
+              <div key={index} className='rounded-full mb-12' style={{ backgroundColor: color, width: '20px', height: '20px' }}></div>
             ))}
           </div>
-          <div className='gap-2'>
-            <h2>Age: {age}</h2>
-            <h2>ٍSex: {sex}</h2>
-            <h2>Type: {type}</h2>
-            <h2>Season: {season}</h2>
-            <h2>Sizes: {size}</h2>
+          <div className=' mb-12'>
+            <h2 className='font-second text-ms p-1'>Age: {age}</h2>
+            <h2 className='font-second text-ms p-1'>ٍSex: {sex}</h2>
+            <h2 className='font-second text-ms p-1'>Type: {type}</h2>
+            <h2 className='font-second text-ms p-1'>Season: {season}</h2>
+            <h2 className='font-second text-ms p-1'>Sizes: {size}</h2>
           </div>
           <button>Send a message to the seller</button>
         </div>
