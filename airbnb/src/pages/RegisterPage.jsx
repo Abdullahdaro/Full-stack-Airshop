a61b51a9276be7c4553294b829eb38b1d45630e5
AbdullahDaro <abode.morx@gmail.com>
2023-05-22 21:27:06 +0300

    add

--- airbnb/src/pages/RegisterPage.jsx
+++ airbnb/src/pages/RegisterPage.jsx
@@ -1,6 +1,7 @@
 import React, {useState } from 'react'
 import { Link } from 'react-router-dom'
 import axios from "axios";
+import { GoogleLogin } from 'react-google-login';
 
 const RegisterPage = () => {
     const [name, setName] = useState("")
@@ -14,16 +15,40 @@ const RegisterPage = () => {
             email,
             password,
         });
-        alert('Registration successful. Now you can log in')
+        alert('Registration successful. Now you can log in') 
       } catch (error) {
         alert ('Registration failed. Please try again later')
       }
       
     }
 
+    const responseGoogle = (response) => {
+      // Handle the response from Google login
+      // Pass the response to your backend for verification and registration
+      // You can call the handleGoogleRegistration function mentioned in the previous response here
+    };
+
+    const googleSuccess = async (res) => {
+       console.log(res);  
+    }
+
+    const googleFailure = (error) => {
+      console.log(error);
+    }
+
   return (
     <div className='mt-4'>
         <h1 className='text-4xl text-center mb-4'>Sign up</h1>
+        <GoogleLogin
+          clientId="720003670148-2rqupqrote7bc33kimqdq0a3lkq68ea3.apps.googleusercontent.com"
+          buttonText="Login with Google"
+          onSuccess={googleSuccess}
+          onFailure={googleFailure}
+          cookiePolicy={'single_host_origin'}
+          render={renderProps => (
+            <button onClick={renderProps.onClick} disabled={renderProps.disabled} className='bg-red-500 text-white px-4 py-2 rounded-md'>Login with Google</button>
+          )}
+        />
         <form onSubmit={registerUser} action="" className='max-w-md mx-auto '>
           <input type="text" placeholder='Your name' value={name} onChange={e => setName(e.target.value)} />
           <input type="email" placeholder='Your@email.com' value={email} onChange={ e => setEmail(e.target.value)}/>
--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.use(cookieParser())
 app.use('/uploads', express.static(path.join(__dirname+ '/uploads')));
 app.use(cors({
   credentials: true,
-  origin: 'http://127.0.0.1:5173'
+  origin: 'https://34a7-46-197-1-226.ngrok-free.app'
 }))
 
 mongoose.connect(process.env.REACT_APP_CONNECTION_URL)
