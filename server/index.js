b65cfd24ea78a0737ef3a809bdc4b513c9823734
AbdullahDaro <abode.morx@gmail.com>
2023-08-14 20:01:59 +0300

    add

--- server/index.js
+++ server/index.js
@@ -328,6 +328,28 @@ class APIfeatures  {
         });
       });
 
+      app.delete('/products/:id', async (req, res) => {
+        const { id } = req.params;
+      
+        try {
+          const product = await Product.findByIdAndDelete(id);
+      
+          if (!product) {
+            return res.status(404).json({ message: 'Product not found' });
+          }
+
+          await UserModel.updateMany(
+            { saved: id },
+            { $pull: { saved: id } }
+          );
+      
+          res.json({ message: 'Product deleted successfully' });
+        } catch (error) {
+          console.error('Error deleting product:', error);
+          res.status(500).json({ message: 'Internal server error' });
+        }
+      });
+
 // homepage products 
       app.get('/homeproducts', async (req,res) => {
         try {
